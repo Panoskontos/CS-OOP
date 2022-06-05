1837145ef63936881142ecdb36bef9a0f8ae8897
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-06-06 01:24:09 +0300

    first commit

---
+++ Book.cs
@@ -0,0 +1,20 @@
+
+
+namespace Lykos
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+    // Constructor
+  public Book(string modelName, string modelAuthor, int modelpages)
+  {
+    title = modelName;
+    author = modelAuthor;
+    pages = modelpages;
+  }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Lykos
+{
+    class Program
+    {
+
+        static void MyMethod(){
+            Console.WriteLine("It's a function");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("Lord knows","Meek",213);
+            
+            Console.WriteLine(b1.title);
+            MyMethod();
+        }
+    }
+}
